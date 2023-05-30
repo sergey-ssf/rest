d5a7aa87aa333d5a8c8cc29b8f5afd0fe955ee8c
Sergey Sakhanskiy <ssf808867@gmail.com>
2023-05-30 09:40:52 +0300

    Working Tests

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -1,12 +1,17 @@
 package ru.netology.rest;
 
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
+
 
 class MobileBankApiTestV1 {
     @Test
-    void shouldReturnDemoAccounts() {
+    void shouldReturnDemoAccounts200Test() {
       // Given - When - Then
       // Предусловия
       given()
@@ -18,4 +23,78 @@ class MobileBankApiTestV1 {
       .then()
           .statusCode(200);
     }
+
+    @Test
+    void shouldReturnJSONSchemaValid2Test() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .body(matchesJsonSchemaInClasspath("accounts.schema.json"));
+    }
+
+    @Test
+    void shouldReturnContentTypeJSONTest3() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .contentType(io.restassured.http.ContentType.JSON);
+    }
+
+    // .header("Content-Type", "application/json; charset=UTF-8")
+    // специализированные проверки - лучше   .contentType(ContentType.JSON)
+
+    @Test
+    void shouldReturnNumberOfObjects4Test() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .body("",hasSize(3));
+    }
+
+    @Test
+    void shouldReturnCurrency5Test() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .body("[0].currency",equalTo("RUB"));
+    }
+
+    @Test
+    void shouldReturnPositiveBalance6Test() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .body("every{ it.balance >= 0 }", Matchers.is(true));
+    }
+
 }
