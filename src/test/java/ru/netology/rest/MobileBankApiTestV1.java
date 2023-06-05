610b9ab822197f635711a7e357264c355bdb13f5
Sergey Sakhanskiy <ssf808867@gmail.com>
2023-06-05 11:46:45 +0300

    Changed RUB to RUS in Json Schema

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -82,7 +82,7 @@ class MobileBankApiTestV1 {
                 .get("/demo/accounts")
                 // Проверки
                 .then()
-                .body("[0].currency",equalTo("RUR"));
+                .body("[0].currency",equalTo("RUB"));
     }
 
     @Test
